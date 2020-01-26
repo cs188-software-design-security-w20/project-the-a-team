86f45e78401e7610b7bb3b28c6b00aad1da70732
Timothy Gu <timothygu99@gmail.com>
2020-01-25 16:33:27 -0800

    Install Postgres

---
+++ backend/models/User.js
@@ -0,0 +1,18 @@
+'use strict';
+const Sequelize = require('sequelize');
+const { sequelize } = require('./database.js');
+
+class User extends Sequelize.Model {}
+
+User.init({
+  email: Sequelize.DataTypes.STRING,
+}, { sequelize, modelName: 'user-test' }); // TODO: change model name
+
+User.sync().catch(err => {
+	process.nextTick(() => {
+		console.error('Failed to sync User db'); // eslint-disable-line no-console
+		throw err;
+	});
+});
+
+module.exports = User;
---
+++ backend/models/database.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('taxmax', 'taximus', '', {
+	host: 'db_postgres',
+	dialect: 'postgres'
+});
+
+exports.sequelize = sequelize;
