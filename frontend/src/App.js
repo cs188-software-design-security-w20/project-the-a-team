bed5b3540dfd88492e7da9a588ec113b4ff659ff
Timothy Gu <timothygu99@gmail.com>
2020-01-31 10:54:20 -0800

    Remove some boilerplate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createMuiTheme, responsiveFontSizes, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import yellow from '@material-ui/core/colors/yellow';
+
+const theme = responsiveFontSizes(createMuiTheme({
+  palette: {
+    primary: yellow,
+    secondary: yellow,
+  },
+}));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-and save to reload. vick
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+    </ThemeProvider>
   );
 }
 
