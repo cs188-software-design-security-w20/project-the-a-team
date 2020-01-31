1a541c5ae50e6da98370da550c366d7b228b799f
Timothy Gu <timothygu99@gmail.com>
2020-01-31 11:42:14 -0800

    Make things line up properly in login page

--- frontend/src/LoginPage.js
+++ frontend/src/LoginPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import loginImg from './images/login.png';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   login: {
     height: '100vh',
     width: '100vw',
@@ -13,28 +13,29 @@ const useStyles = makeStyles(() => ({
     display: 'flex',
   },
 
-  subtitle: {
-    margin: '0px',
+  container: {
     position: 'absolute',
-    top: '57.5vh',
-    fontSize: '2vw',
+    top: '57vh',
     paddingLeft: '1vw',
+  },
+
+  subtitle: {
+    margin: 0,
+    lineHeight: 1,
+    fontSize: '2vw',
   },
 
   title: {
-    margin: '0px',
-    position: 'absolute',
-    top: '61vh',
+    margin: 0,
+    lineHeight: 0.8,
     fontSize: '6.3vw',
-    paddingLeft: '1vw',
+    textTransform: 'uppercase',
   },
 
   loginButton: {
-    position: 'absolute',
+    marginTop: theme.spacing(2),
     width: '20vw',
-    top: '73vh',
     fontSize: '1vw',
-    marginLeft: '1vw',
   },
 }));
 
@@ -43,9 +44,11 @@ function LoginPage() {
 
   return (
     <div className={classes.login}>
-      <h2 className={classes.subtitle}>The easiest way to do easy taxes.</h2>
-      <h1 className={classes.title}>TAXIMUS MAXIMUS</h1>
-      <button type="button" className={classes.loginButton}>Sign in with Google</button>
+      <div className={classes.container}>
+        <h2 className={classes.subtitle}>The easiest way to do easy taxes.</h2>
+        <h1 className={classes.title}>Taximus Maximus</h1>
+        <button type="button" className={classes.loginButton}>Sign in with Google</button>
+      </div>
     </div>
   );
 }
