f47857baa271e8f9dcb5592d19675520ad7c66f9
Jason Lai <jasonyslai1@gmail.com>
2020-02-08 16:15:09 -0800

    Added tax calculation logic

--- backend/service/calculation.js
+++ backend/service/calculation.js
@@ -2,18 +2,18 @@
 
 const query = require('./query.js');
 
-const calcTotalWages = (fw2) => {
-  let total = 0;
+function calcTotalWages(fw2) {
+  let total = 0n;
   for (const form of fw2) {
     if (form.income) {
       total += (BigInt(form.income));
     }
   }
-  return (total / 100);
-};
+  return total;
+}
 
-const calcTotalTaxExemptInterest = (f1099int, f1099div) => {
-  let total = 0;
+function calcTotalTaxExemptInterest(f1099int, f1099div) {
+  let total = 0n;
   for (const form of f1099int) {
     if (form.taxExemptInterest) {
       total += (BigInt(form.taxExemptInterest));
@@ -24,32 +24,70 @@ const calcTotalTaxExemptInterest = (f1099int, f1099div) => {
       total += (BigInt(form.exemptInterestDiv));
     }
   }
-  return (total / 100);
-};
+  return total;
+}
 
-const calcTotalTaxableInterest = (f1099int) => {
-  let total = 0;
+function calcTotalTaxableInterest(f1099int) {
+  let total = 0n;
   for (const form of f1099int) {
     if (form.income) {
       total += (BigInt(form.income));
     }
   }
-  return (total / 100);
-};
+  return total;
+}
 
-const calculate = (user) => {
+function calcTotalQualDiv(f1099div) {
+  let total = 0n;
+  for (const form of f1099div) {
+    if (form.qualDividends) {
+      total += (BigInt(form.qualDividends));
+    }
+  }
+  return total;
+}
+
+function calcTotalOrdDiv(f1099div) {
+  let total = 0n;
+  for (const form of f1099div) {
+    if (form.ordDividends) {
+      total += (BigInt(form.ordDividends));
+    }
+  }
+  return total;
+}
+
+function bigIntToString(bigInt, roundUpToZero) {
+  let num = bigInt;
+  const isNeg = num < 0n;
+  if (num === 0n || (isNeg && roundUpToZero)) {
+    return '-0-';
+  }
+
+  if (isNeg) num = -num;
+  const dollarPart = String(num / 100n);
+  const centPart = String(num % 100n).padStart(2, '0');
+
+  if (isNeg) {
+    return `(${dollarPart}.${centPart})`;
+  }
+  return `${dollarPart}.${centPart}`;
+}
+
+function calculate(user) {
   const forms = query.getUserData(user);
   if (forms === null) {
     console.log('there are no forms for this user'); // eslint-disable-line
     return null;
   }
-  const [
-    dep1,
-    dep2,
-    dep3,
-    dep4,
-  ] = forms.dependents;
+  const [dep1, dep2, dep3, dep4] = forms.dependents;
   const isSingle = !(forms.taxinfo.spouseName && forms.taxinfo.spouseSsn);
+  const line1 = calcTotalWages(forms.fw2);
+  const line2a = calcTotalTaxExemptInterest(forms.f1099int, forms.f1099div);
+  const line2b = calcTotalTaxableInterest(forms.f1099int);
+  const line3a = calcTotalQualDiv(forms.f1099div);
+  const line3b = calcTotalOrdDiv(forms.f1099div);
+  const line7b = line1 + line2b + line3b;
   return {
     firstName: forms.taxinfo.firstName + forms.taxinfo.middleName[0],
     lastName: forms.taxinfo.lastName,
@@ -76,10 +114,13 @@ const calculate = (user) => {
     dep4SSN: dep4 ? dep4.ssn : '',
     dep4Rel: dep4 ? dep4.relation : '',
     dep4ChildCredit: dep4 ? dep4.childCredit : false,
-    l1: calcTotalWages(forms.fw2),
-    l2a: calcTotalTaxExemptInterest(forms.f1099int, forms.f1099div),
-    l2b: calcTotalTaxableInterest(forms.f1099int),
+    l1: bigIntToString(line1),
+    l2a: bigIntToString(line2a),
+    l2b: bigIntToString(line2b),
+    l3a: bigIntToString(line3a),
+    l3b: bigIntToString(line3b),
+    l7b: bigIntToString(line7b),
   };
-};
+}
 
 module.exports = { calculate };
