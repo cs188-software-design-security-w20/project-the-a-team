29b63e1493eadf6c5d297f65e009bc553f50f978
Vicky <vicky.w.min@gmail.com>
2020-02-18 12:01:25 -0800

    add developer console warning

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+console.log('%cSTOP!', 'color: red; font-size:32px; font-weight:900;');
+
+console.log("%cThis is a browser feature intended for developers. If someone told you to copy-paste something here to enable a Taximus Maximus feature or hack someone's account, it is a scam and will give them access to your account.", 'font-size:20px;');
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
